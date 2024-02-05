50ec7dce82a9ad82c9537ad14fb12b57cfdf89e7
ronanfitz <ronanfitz13@gmail.com>
2024-02-05 11:02:56 -0800

    Add backend support for login and tokenization/authorization

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,17 @@
-import express from 'express';
+import express from "express";
 import { config } from "dotenv";
 import morgan from "morgan";
-import appRouter from './routes/index.js';
+import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 config();
 
 const app = express();
 
 // middlewares
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // logs api call information
 app.use(morgan("dev"));
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,26 +1,131 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
+import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 
 // get all users from the database
-export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const users = await User.find();
-        return res.status(200).json({ message: "OK", users })
-    } catch (error) {
-        return res.status(200).json({message: "ERROR", cause: error.message})
-    }
-}
+export const getAllUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json({ message: "OK", users });
+  } catch (error) {
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
 
-// get all users from the database
-export const userSignup = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { name, email, password } = req.body;
-        const hashedPassword = await hash(password, 10);
-        const user = new User({ name, email, password: hashedPassword });
-        await user.save();
-        return res.status(200).json({ message: "OK", id: user._id.toString() })
-    } catch (error) {
-        return res.status(200).json({message: "ERROR", cause: error.message})
+// create a new user
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(401).send("User already registered");
+    const hashedPassword = await hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+
+    // create token and store cookie
+    res.clearCookie(COOKIE_NAME, {
+      path: "/",
+      domain: "localhost",
+      httpOnly: true,
+      signed: true,
+    });
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
+    return res
+      .status(200)
+      .json({ message: "OK", name: user.name, email: user.email });
+  } catch (error) {
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
+
+// login as a user
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).send("User not registered");
     }
-}
+
+    const isPasswordCorrect = await compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(403).send("Incorrect password");
+    }
+
+    // create token and store cookie
+    res.clearCookie(COOKIE_NAME, {
+      path: "/",
+      domain: "localhost",
+      httpOnly: true,
+      signed: true,
+    });
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
+    return res
+      .status(200)
+      .json({ message: "OK", name: user.name, email: user.email });
+  } catch (error) {
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
+
+// verify if a user is logged in
+export const verifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await User.findById(res.locals.jwtData.id);
+
+    if (!user) {
+      return res.status(401).send("User not registered or token malfunction");
+    }
+
+    if (user._id.toString() !== res.locals.jwtData.id) {
+      return res.status(401).send("Permissions didn't match");
+    }
+
+    return res
+      .status(200)
+      .json({ message: "OK", name: user.name, email: user.email });
+  } catch (error) {
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
