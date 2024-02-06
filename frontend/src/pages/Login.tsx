f5367149286f53cc00710b527ea7188e7a86809c
ronanfitz <ronanfitz13@gmail.com>
2024-02-06 15:43:34 -0800

    Fix errors in files

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -33,7 +33,7 @@ const Login = () => {
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
-        <img src="airobot.png" alt="Robot" style={{ width: "400" }} />
+        <img src="airobot.png" alt="Robot" style={{ width: "400px" }} />
       </Box>
       <Box
         display={"flex"}
